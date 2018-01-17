db13d45ebc36d8edda1b657202ce4985669794c4
logan tanous and roger yee <roger@>
2018-01-16 17:07:02 -0800

    did things

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,63 @@
 $(document).ready(function() {
   var data = [];
-  var length = 10; // user defined length
+  var length = 9; // user defined length
   var rownumber = 0;
   var vNumber = 0;
   var hNumber = 0;
+  length = length + 1;
   for(var k = 0; k < length; k++) {
     vNumber = 0;
     hNumber = 0;
-    for(var i = 0; i < length; i++) {
+    for(var i = 0; i < length-1; i++) {
       data.push("1");
-      $(".container").append("<div class=\"lrSides unclicked v"+rownumber+"-"+vNumber+" \"></div><div class=\"tbSides unclicked h"+rownumber+"-"+hNumber+"\"></div>");
+      $(".container").append("<div id=\"v"+rownumber+"-"+vNumber+"\"class=\"lrSides unclicked v"+rownumber+"-"+vNumber+" "+rownumber+"\"></div><div id=\"h"+rownumber+"-"+vNumber+"\"class=\"tbSides unclicked h"+rownumber+"-"+hNumber+"\"></div>");
       vNumber++;
       hNumber++;
       };
-      $(".container").append("<div class=\"lrSides unclicked v"+rownumber+"-"+vNumber+"\"></div>");
-    $(".container").append("<br>");
-    rownumber++;
+      $(".container").append("<div id=\"v"+rownumber+"-"+vNumber+"\" class=\"lrSides unclicked v"+rownumber+"-"+vNumber+" "+rownumber+"\"></div>");
+      if (rownumber<length){
+        $(".container").append("<br>");
+      }
+      rownumber++;
   };
-  for(var k = 0; k < length; k++) {
-    $(".container").append("<div class=\"fSides\"></div><div class=\"tbSides unclicked h"+length+"-"+k+"\"></div>");
-  }
   $(".unclicked").click(function(event) {
-    $(this).css('background', '#000')
-  });
+    $(this).css('background', '#000').addClass('clicked');
+    $(".10").css('background', '#fff').removeClass('clicked');
+    //get id name
+    var idName = $(this).attr('id');
 
+    var firstNumber = idName.substring(1,2);
+    var lastNumber = idName.substring(3,4);
+    //alert(firstNumber+" "+lastNumber);
+    if (idName.substring(0,1) == "v") {
+        if($("#v1-1").hasClass("unclicked") == true) {
+          alert("test");
+        }
+    }
+    //if (idName.substring(0,1) == "h") {}
+
+  });
+  var lessThanLength = length-1;
+  $("."+lessThanLength).removeClass('unclicked');
 $(".fSides:first").css('margin-left', '-10px');
 
 
  });
+
+ /*
+ V(x|y) =
+ > V[x|(y-1)]
+ > V[x|(y+1)]
+ > H[x|(y-1)]
+ > H[(x+1)|(x-1)]
+ > H(x|y)
+ > H[(x+1)|y]
+ ------------
+ H(x|y) =
+ > H[(x-1)|y]
+ > H[(x+1)|y]
+ > V[(x-1)|y]
+ > V[(x-1)|(y+1)]
+ > V(x|y)
+ > V[x|(y+1)]
+ */
