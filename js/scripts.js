fab8b8e8085476c8fa8b213847edf8fa16fd6de7
Logan Tanous and Mariah Shipley <logan@>
2018-01-18 10:40:38 -0800

    add format for list

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,27 @@
 $(document).ready(function() {
-  var data = [];
-  var length = 9; // user defined length
-  var rownumber = 0;
-  var vNumber = 0;
-  var hNumber = 0;
-  length = length + 1;
-  for(var k = 0; k < length; k++) {
-    vNumber = 0;
-    hNumber = 0;
-    for(var i = 0; i < length-1; i++) {
-      data.push("1");
-      $(".container").append("<div id=\"v"+rownumber+"-"+vNumber+"\"class=\"lrSides unclicked v"+rownumber+"-"+vNumber+" "+rownumber+"\"></div><div id=\"h"+rownumber+"-"+vNumber+"\"class=\"tbSides unclicked h"+rownumber+"-"+hNumber+"\"></div>");
-      vNumber++;
-      hNumber++;
-      };
-      $(".container").append("<div id=\"v"+rownumber+"-"+vNumber+"\" class=\"lrSides unclicked v"+rownumber+"-"+vNumber+" "+rownumber+"\"></div>");
-      if (rownumber<length){
-        $(".container").append("<br>");
-      }
-      rownumber++;
-  };
-  $(".unclicked").click(function(event) {
-    $(this).css('background', '#000').addClass('clicked');
-    $(".10").css('background', '#fff').removeClass('clicked');
-    //get id name
-    var idName = $(this).attr('id');
 
-    var firstNumber = idName.substring(1,2);
-    var lastNumber = idName.substring(3,4);
-    //alert(firstNumber+" "+lastNumber);
-    if (idName.substring(0,1) == "v") {
-        $(".unclicked").click(function(event) {
-          if ($(".clicked")[0]){//it has the class of clicked
-            alert("test");
-          }
-        });
-    }
-    //if (idName.substring(0,1) == "h") {}
+  var list = [];//create a blank array
+
+  //when #addToList is clicked
+  $( "#addToList" ).click(function() {
+
+    //add to list input's value
+    list.push($("input").val());
+    //clear input's value
+    $("input").val('');
 
   });
-  var lessThanLength = length-1;
-  $("."+lessThanLength).removeClass('unclicked');
-$(".fSides:first").css('margin-left', '-10px');
 
+  //now that list has items we are going to loop through and add them to the page
+
+  //when #displayToPage is clicked
+  $( "#displayToPage" ).click(function() {
+
+    //loop through all items in the list array
+    for(var i = 0; i < list.length ; i++) {
+      //add (append) all items in list to the container
+      $(".container").append(list[i]+"<br>");
+    };
+  });
 
  });
-
- /*
- V(x|y) =
- > V[x|(y-1)]
- > V[x|(y+1)]
- > H[x|(y-1)]
- > H[(x+1)|(x-1)]
- > H(x|y)
- > H[(x+1)|y]
- ------------
- H(x|y) =
- > H[(x-1)|y]
- > H[(x+1)|y]
- > V[(x-1)|y]
- > V[(x-1)|(y+1)]
- > V(x|y)
- > V[x|(y+1)]
- */
